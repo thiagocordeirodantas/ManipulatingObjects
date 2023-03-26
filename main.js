fd6fa6008e64d12b0ae39852b359c961973a9112
Thiago Cordeiro <thiagocordeirodantasw@gmail.com>
2023-03-25 23:06:04 -0300

    criando um cliente

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 let listaDeItens = []  
 
+
+
+let  cliente = {
+    nome: 'Mariana', 
+    idade: 35,
+    genero: 'Feminino',
+    estado: 'Pernambuco',
+
+    mostrarCliente: function(){
+        alert(`Olá, meu nome é Mariana e moro no estado de  ${cliente.estado}`)
+    }
+ }
+
+ cliente.mostrarCliente()
